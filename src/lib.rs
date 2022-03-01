7fe0c31613b1adafbabc941e0037cb7c43db55ff
josh rotenberg <joshrotenberg@users.noreply.github.com>
2022-03-01 13:27:41 -0800

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,154 @@
+//! Oso Authorization middleware
+
+use std::{future::Future, rc::Rc, sync::Arc};
+
+use actix_web::{
+    body::{EitherBody, MessageBody},
+    dev::{Service, ServiceRequest, ServiceResponse, Transform},
+    Error,
+};
+use actix_web::Result;
+use futures_util::future::{self, FutureExt as _, LocalBoxFuture};
+use oso::Oso;
+
+pub mod errors;
+
+pub struct OsoAuthorization<F> {
+    pub authorize_fn: Arc<F>,
+    // oso: Arc<Oso>,
+}
+
+impl<F, O> OsoAuthorization<F>
+    where
+        F: Fn(ServiceRequest, Oso) -> O,
+        O: Future<Output=Result<ServiceRequest, Error>>,
+{
+    pub fn with_fn(authorize_fn: F) -> OsoAuthorization<F> {
+        OsoAuthorization {
+            authorize_fn: Arc::new(authorize_fn),
+            // oso: Arc::new(Oso::new()),
+        }
+    }
+}
+
+impl<S, B, F, O> Transform<S, ServiceRequest> for OsoAuthorization<F>
+    where
+        S: Service<ServiceRequest, Response=ServiceResponse<B>, Error=Error> + 'static,
+        S::Future: 'static,
+        F: Fn(ServiceRequest, Oso) -> O + 'static,
+        O: Future<Output=Result<ServiceRequest, Error>> + 'static,
+        B: MessageBody + 'static,
+{
+    type Response = ServiceResponse<EitherBody<B>>;
+    type Error = Error;
+    type Transform = OsoAuthorizationMiddleware<S, F>;
+    type InitError = ();
+    type Future = future::Ready<Result<Self::Transform, Self::InitError>>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        future::ok(OsoAuthorizationMiddleware {
+            service: Rc::new(service),
+            authorize_fn: self.authorize_fn.clone(),
+        })
+    }
+}
+
+pub struct OsoAuthorizationMiddleware<S, F> {
+    service: Rc<S>,
+    authorize_fn: Arc<F>,
+}
+
+impl<S, B, F, O> Service<ServiceRequest> for OsoAuthorizationMiddleware<S, F>
+    where
+        S: Service<ServiceRequest, Response=ServiceResponse<B>, Error=Error> + 'static,
+        F: Fn(ServiceRequest, Oso) -> O + 'static,
+        O: Future<Output=Result<ServiceRequest, Error>> + 'static,
+        S::Future: 'static,
+        B: MessageBody + 'static,
+{
+    type Response = ServiceResponse<EitherBody<B>>;
+    type Error = Error;
+    type Future = LocalBoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    actix_service::forward_ready!(service);
+
+    fn call(&self, req: ServiceRequest) -> Self::Future {
+        let authorize_fn = Arc::clone(&self.authorize_fn);
+        let service = Rc::clone(&self.service);
+
+        async move {
+            let oso = req.app_data::<Oso>().map(|o| o.clone());
+            if oso.is_none() {
+                return Err(actix_web::error::ErrorUnauthorized("No Oso available"));
+            }
+
+            let req = authorize_fn(req, oso.unwrap()).await?;
+            service.call(req).await.map(|res| res.map_into_left_body())
+        }
+            .boxed_local()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use actix_service::{into_service, Service};
+    use actix_web::{error, HttpResponse};
+    use actix_web::test::TestRequest;
+
+    use super::*;
+
+    #[actix_rt::test]
+    async fn test_oso_middleware_is_ok() {
+        let middleware = OsoAuthorizationMiddleware {
+            service: Rc::new(into_service(|req: ServiceRequest| async move {
+                Ok::<ServiceResponse, _>(req.into_response(HttpResponse::Ok().finish()))
+            })),
+            authorize_fn: Arc::new(|req, _oso| async { Ok(req) }),
+        };
+
+        let req = TestRequest::get().app_data(Oso::new()).to_srv_request();
+        let f = middleware.call(req).await;
+
+        let _res = futures_util::future::lazy(|cx| middleware.poll_ready(cx)).await;
+
+        assert!(f.is_ok());
+    }
+
+    #[actix_rt::test]
+    async fn test_oso_middleware_is_not_ok() {
+        let middleware = OsoAuthorizationMiddleware {
+            service: Rc::new(into_service(|req: ServiceRequest| async move {
+                Ok::<ServiceResponse, _>(req.into_response(HttpResponse::Ok().finish()))
+            })),
+            authorize_fn: Arc::new(|_req, _oso| async {
+                Err(error::ErrorUnauthorized("none shall pass"))
+            }),
+        };
+
+        let req = TestRequest::get().app_data(Oso::new()).to_srv_request();
+        let f = middleware.call(req).await;
+
+        let _res = futures_util::future::lazy(|cx| middleware.poll_ready(cx)).await;
+
+        assert!(f.is_err());
+    }
+
+    #[actix_rt::test]
+    async fn test_oso_middleware_is_not_ok_no_oso() {
+        let middleware = OsoAuthorizationMiddleware {
+            service: Rc::new(into_service(|req: ServiceRequest| async move {
+                Ok::<ServiceResponse, _>(req.into_response(HttpResponse::Ok().finish()))
+            })),
+            authorize_fn: Arc::new(|_req, _oso| async {
+                Err(error::ErrorUnauthorized("none shall pass"))
+            }),
+        };
+
+        let req = TestRequest::get().to_srv_request();
+        let f = middleware.call(req).await;
+
+        let _res = futures_util::future::lazy(|cx| middleware.poll_ready(cx)).await;
+
+        assert!(f.is_err());
+    }
+}
---
+++ tests/test_oso_authz.rs
@@ -0,0 +1,88 @@
+use actix_web::{
+    App, HttpResponse, Responder, test,
+    web,
+};
+use oso::{Oso, PolarClass};
+
+use actix_web_oso::OsoAuthorization;
+
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[derive(Debug, PolarClass)]
+struct User {
+    #[polar(attribute)]
+    name: String,
+}
+
+#[actix_web::test]
+async fn test_oso_authz_success() {
+    let mut oso = Oso::new();
+    oso.register_class(User::get_polar_class()).unwrap();
+    oso.load_str(r#"allow(actor, _action, _resource) if actor matches User{name: "alice"};"#).unwrap();
+
+    let authz = OsoAuthorization::with_fn(|req, oso| async move {
+        if oso.is_allowed(User { name: "alice".to_string() }, "action", "resource").unwrap() {
+            Ok(req)
+        } else {
+            Err(actix_web::error::ErrorUnauthorized("no sir"))
+        }
+    });
+    let app = test::init_service(
+        App::new()
+            .app_data(oso.clone())
+            .wrap(authz)
+            .route("/", web::get().to(hello)),
+    )
+        .await;
+    let req = test::TestRequest::default().to_request();
+    let resp = test::call_service(&app, req).await;
+    assert!(resp.status().is_success());
+}
+
+#[actix_web::test]
+#[should_panic]
+async fn test_oso_authz_failure() {
+    let mut oso = Oso::new();
+    oso.register_class(User::get_polar_class()).unwrap();
+    oso.load_str(r#"allow(actor, _action, _resource) if actor matches User{name: "alice"};"#).unwrap();
+
+    let authz = OsoAuthorization::with_fn(|req, oso| async move {
+        if oso.is_allowed(User { name: "lice".to_string() }, "action", "resource").unwrap() {
+            Ok(req)
+        } else {
+            Err(actix_web::error::ErrorUnauthorized("no sir"))
+        }
+    });
+    let app = test::init_service(
+        App::new()
+            .app_data(oso.clone())
+            .wrap(authz)
+            .route("/", web::get().to(hello)),
+    )
+        .await;
+    let req = test::TestRequest::default().to_request();
+    test::call_service(&app, req).await;
+}
+
+#[actix_web::test]
+#[should_panic]
+async fn test_missing_oso() {
+
+    let authz = OsoAuthorization::with_fn(|req, oso| async move {
+        if oso.is_allowed(User { name: "lice".to_string() }, "action", "resource").unwrap() {
+            Ok(req)
+        } else {
+            Err(actix_web::error::ErrorUnauthorized("no sir"))
+        }
+    });
+    let app = test::init_service(
+        App::new()
+            .wrap(authz)
+            .route("/", web::get().to(hello)),
+    )
+        .await;
+    let req = test::TestRequest::default().to_request();
+    test::call_service(&app, req).await;
+}
