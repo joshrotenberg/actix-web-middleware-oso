42eb2b5a077a8437b830be0f7f8df6f1a544e2e7
josh rotenberg <joshrotenberg@users.noreply.github.com>
2022-03-15 16:05:08 -0700

    simpllify example

--- examples/main.rs
+++ examples/main.rs
@@ -1,31 +1,40 @@
+use actix_web::{
+    App, Error, HttpResponse, HttpServer, middleware, web,
+};
 use actix_web::dev::ServiceRequest;
-use actix_web::{middleware, web, App, Error, HttpServer};
-use oso::{Oso, PolarClass};
+use actix_web::error::ErrorUnauthorized;
+use oso::Oso;
 
 use actix_web_middleware_oso::OsoAuthorization;
 
-#[derive(Debug, PolarClass)]
-struct User {
-    name: String,
-}
+async fn authorize(req: ServiceRequest, oso: Oso) -> Result<ServiceRequest, Error> {
+    let action = req.method().to_string().to_uppercase();
+    let resource = req.path();
 
-async fn authorizer(req: ServiceRequest, _oso: Oso) -> Result<ServiceRequest, Error> {
-    Ok(req)
+    log::info!("checking access to {} with {}", resource, action);
+
+    match oso.is_allowed("_actor", action, resource) {
+        Ok(true) => Ok(req),
+        _ => Err(ErrorUnauthorized("not allowed")),
+    }
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
     HttpServer::new(|| {
         let mut oso = Oso::new();
-        oso.register_class(User::get_polar_class());
-        let authz = OsoAuthorization::with_fn(oso, authorizer);
+        oso.load_str(r#"allow(_actor, action, resource) if action = "GET" and resource.starts_with("/ok/");"#)
+            .unwrap();
+
+        let authz = OsoAuthorization::new(oso, authorize);
         App::new()
-            // .app_data(oso.clone())
             .wrap(middleware::Logger::default())
             .wrap(authz)
-            .service(web::resource("/").to(|| async { "Test\r\n" }))
+            .default_service(web::to(|| HttpResponse::Ok()))
     })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
